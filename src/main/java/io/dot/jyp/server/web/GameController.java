c2edbc955519fbdf42d19af31abb1d2babbd199b
heosang <57525618+heosang@users.noreply.github.com>
2021-02-20 08:36:35 +0900

    Update GameController.java

--- src/main/java/io/dot/jyp/server/web/GameController.java
+++ src/main/java/io/dot/jyp/server/web/GameController.java
@@ -1,7 +1,26 @@
 package io.dot.jyp.server.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.dot.jyp.server.application.GameApplicationService;
+import io.dot.jyp.server.application.GroupApplicationService;
+import io.dot.jyp.server.application.dto.GameStartRequest;
+import io.dot.jyp.server.application.dto.GameStartResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
+@RequestMapping("/api/v1/game")
 public class GameController {
+    private final GameApplicationService gameApplicationService;
+
+    public GameController(GameApplicationService gameApplicationService) {
+        this.gameApplicationService = gameApplicationService;
+    }
+
+    @PostMapping("/start")
+    @ResponseStatus(value = HttpStatus.OK)
+    public GameStartResponse gameStart(@RequestBody final GameStartRequest request) {
+        return GameStartResponse.of("임시 이름", "중식");
+    }
 }
