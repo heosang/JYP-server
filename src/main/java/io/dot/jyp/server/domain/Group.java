c4a5f3559bfc0c99fd19f75d2892b48d0eb75623
heosang <57525618+heosang@users.noreply.github.com>
2021-02-21 22:28:36 +0900

    Update Group.java

--- src/main/java/io/dot/jyp/server/domain/Group.java
+++ src/main/java/io/dot/jyp/server/domain/Group.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Entity
 @Table(name = "groups")
@@ -25,8 +26,40 @@ public class Group {
     @JoinColumn(name = "group_id", foreignKey = @ForeignKey(name = "fk_group_id"))
     private List<Diner> diners = new ArrayList<>();
 
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", foreignKey = @ForeignKey(name = "fk_group_id"))
+    private List<Account> accounts = new ArrayList<>();
+
     @Column(name = "created_at")
     private LocalDateTime created_at;
 
+    private Group(
+            List<Diner> diners,
+            Account account,
+            LocalDateTime created_at
+    ){
+        this.diners=diners;
+        this.accounts.add(account);
+        this.created_at = created_at;
+    }
+
+    public static Group groupCreate(
+            List<Diner> diners,
+            Account account
+    ){
+        return new Group(
+                diners,
+                account,
+                LocalDateTime.now()
+        );
+    }
+    public void entrance(
+            List<Diner> diners,
+            Account account
+    ){
+        for(Diner diner : diners){ this.diners.add(diner); }
+        this.accounts.add(account);
+    }
+    public void exit(Account account){ this.accounts.remove(account); }
 
 }
